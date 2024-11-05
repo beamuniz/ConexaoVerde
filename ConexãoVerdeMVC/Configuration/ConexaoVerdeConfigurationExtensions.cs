d5a6e656505c61a49872ed27491e51db0adb3d0b
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2024-11-05 20:55:34 -0300

    ConexaoVerdeConfigurationExtensions.cs

---
+++ ConexãoVerdeMVC/Configuration/ConexaoVerdeConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ConexãoVerdeMVC.Business.Interfaces;
+using ConexãoVerdeMVC.Business.Services;
+
+namespace ConexãoVerdeMVC.Configuration;
+
+public static class ConexaoVerdeConfigurationExtensions
+{
+    public static void AddConexaoVerdeServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioBusiness>();
+        services.AddScoped<UsuarioBusiness>();
+    }
+}
--- ConexãoVerdeMVC/Program.cs
+++ ConexãoVerdeMVC/Program.cs
@@ -1,6 +1,5 @@
 using ConexãoVerdeAppData.Context;
-using ConexãoVerdeMVC.Business.Interfaces;
-using ConexãoVerdeMVC.Business.Services;
+using ConexãoVerdeMVC.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DbContextConfig>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IUsuarioBusiness, UsuarioBusiness>();
+builder.Services.AddConexaoVerdeServices();
 
 var app = builder.Build();
 
