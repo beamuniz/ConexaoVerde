eda07b8ddc7a6d2a0bf78f826a628a24b79d1ca0
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2024-11-24 12:38:40 -0300

    Tag asp-for e tratamento de senha em usuário

--- ConexaoVerde.Web/Models/UsuarioModel.cs
+++ ConexaoVerde.Web/Models/UsuarioModel.cs
@@ -1,12 +1,14 @@
-namespace ConexaoVerde.Web.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ConexaoVerde.Web.Models;
+
+public class UsuarioModel
 {
-    public class UsuarioModel
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }     
-        public string Telefone { get; set; }   
-        public byte[]? FotoPerfil { get; set; }  
-        public string Perfil { get; set; }
-    }
+    public int Id { get; set; }
+    public string Email { get; set; }
+    [Required(ErrorMessage = "A senha é obrigatória.")]
+    public string Senha { get; set; }
+    public string Telefone { get; set; }
+    public byte[]? FotoPerfil { get; set; }
+    public string Perfil { get; set; }
 }
