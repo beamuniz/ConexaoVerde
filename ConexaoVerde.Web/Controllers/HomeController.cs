1173c3638e543b44d955edfe13543023ec7b6d50
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2024-12-04 13:19:30 -0300

    Ajuste na autenticação do _Layout.cshtml

--- ConexaoVerde.Web/Controllers/HomeController.cs
+++ ConexaoVerde.Web/Controllers/HomeController.cs
@@ -1,44 +1,17 @@
 using System.Diagnostics;
 using ConexaoVerde.Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 namespace ConexaoVerde.Web.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     // Página inicial (Index)
     public IActionResult Index()
     {
-        // Verificar se o usuário está autenticado
-        if (User.Identity?.IsAuthenticated == true)
-        {
-            // Passar informações para a View
-            ViewData["IsLoggedIn"] = true;
-            ViewData["UserName"] = User.Identity.Name; // Nome do usuário logado
-        }
-        else
-        {
-            ViewData["IsLoggedIn"] = false;
-        }
-
         return View();
     }
 
-    // Página de privacidade
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    // Página de erro
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
